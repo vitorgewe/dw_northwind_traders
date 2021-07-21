a80e48bb12f723902d31819ee8c9a2b22598b580
vitorgewe <vitorgewe@gmail.com>
2021-07-21 09:29:10 -0300

    Added date to fact order details table

--- models/marts/fact_order_details.sql
+++ models/marts/fact_order_details.sql
@@ -3,6 +3,10 @@ with
         select *
         from {{ ref('dim_products') }}
     )
+    , orders as (
+        select *
+        from {{ ref('fact_orders') }}
+    )
     , prod_order_details as (
         select
             /* Primary key */
@@ -12,6 +16,7 @@ with
             , products.product_sk as product_fk
 
             /* Selected columns in prod tables */
+            , orders.order_date
             , order_details.unit_price
             , order_details.quantity
             , order_details.discount
@@ -20,6 +25,8 @@ with
         from {{ ref('stg_fact_order_details') }} as order_details
         left join products
         on products.product_id = order_details.product_id
+        left join orders
+        on orders.order_id = order_details.order_id
     )
 
 select * from prod_order_details
